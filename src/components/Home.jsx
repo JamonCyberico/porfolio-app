7090a0984b035bb9d736c82c93ccee2a9c6857f6
AnthonyEP <anthony.ajami@epita.fr>
2024-01-08 18:58:32 +0100

    add skills section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <p className='text-pink-600 text-4xl font-bold'>Hello, my name is</p>
         <h1 className='text-white text-7xl font-bold'>Anthony Ajami</h1>
         <h2 className='text-6xl text-[#8892b0] font-bold'>I'm a full stack developer</h2>
-        <p className='text-[#8892b0] font-light'>I have an Msc. in Software engineering from EPITA. I have a passion for building web applications and solving complex problems.</p>
+        <p className='text-[#8892b0] font-light mt-4'>I have an Msc. in Software engineering from EPITA. I have a passion for building web applications and solving complex problems. I'm currently looking for a full-time position as a software engineer.</p>
         <div>
           <button className='text-white border border-white mt-8 px-6 py-3 rounded hover:bg-pink-600 hover:text-primary duration-200'>
             Check out my work
